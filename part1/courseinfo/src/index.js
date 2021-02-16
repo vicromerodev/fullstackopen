935f4705a5f4636609404ae9ef0831688e6714d9
Vic Romero <vromerocomino@gmail.com>
2021-02-16 08:24:52 +0100

    Fixed typo part1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Header = ({name}) => <h1>{name}</h1>
+const Header = ({ name }) => <h1>{name}</h1>;
 
-const Content = ({parts}) => parts.map(part => <p>{part.name} {part.exercises}</p>)
+const Content = ({ parts }) =>
+  parts.map((part) => (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  ));
 
-const Total = ({parts}) => <p>Number of exercises {parts.reduce((acc, obj) => acc + obj.exercises, 0)}</p>
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((acc, obj) => acc + obj.exercises, 0)}
+  </p>
+);
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10 
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name:'State of a component',
-        exercises: 14
-      }
-    ]
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <>
-      <Header course={course.name} />
+      <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </>
-  )
-}
-ReactDOM.render(<App />, document.getElementById('root'))
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
